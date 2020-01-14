f379eb6123d0459e87a446d8a59126a8b30b9e64
AMZ <amartinezundmx@gmail.com>
2020-01-13 18:42:24 -0800

    Finished email uniqueness and password hashing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  get '/home', to: 'sessions#home'
+  get '/login', to: 'sessions#login'
+  root 'sessions#home'
 end
