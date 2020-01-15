d78ad6fdb1b55449a9ef46858b79719ef0aeab42
AMZ <amartinezundmx@gmail.com>
2020-01-14 17:22:44 -0800

    Working sign up. Needs complete validation

--- Gemfile
+++ Gemfile
@@ -18,7 +18,6 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 
-gem 'bcrypt', '~> 3.1'
 
 gem 'bootstrap', '~> 4.4', '>= 4.4.1'
 # Use Redis adapter to run Action Cable in production
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
+  get '/signup', to: 'usuarios#new'
   get '/home', to: 'sessions#home'
   get '/login', to: 'sessions#login'
-  root 'sessions#home'
+  resources :usuarios
 end
